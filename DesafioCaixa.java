4a6ce4652b929bf0904806f1b8d98aa40ac6fad8
Vieir44 <159278053+Vieir44@users.noreply.github.com>
2024-08-14 16:31:24 -0300

    segundo commit

--- DesafioCaixa.java
+++ DesafioCaixa.java
@@ -1,46 +1,73 @@
 package desafio;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DesafioCaixa {
 
 	public static void main(String[] args) {
-		ArrayList<String> ing = new ArrayList();
-		ArrayList<Double> precos = new ArrayList();
-		Scanner ler = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> nomesProdutos = new ArrayList<>();
+		ArrayList<Double> valoresProdutos = new ArrayList<>();
 
-		String produtos;
-		double valor;
-	
+		while (true) {
+			System.out.println("Digite o nome do produto (ou 'X' para encerrar):");
+			String nomeProduto = scanner.nextLine();
 
-		while(0) {
+			if (nomeProduto.equalsIgnoreCase("X")) {
+				break;
+			}
+			double valorProduto = 0;
+			boolean valorValido = false;
 
-			System.out.println("Quais são os produtos que você deseja adicionar");
-			do {
-				produtos = ler.nextLine();
-
-				if(produtos != "X") {
-					ing.add(produtos);
-
+			while (!valorValido) {
+				System.out.println("Digite o valor do produto:");
+				try {
+					valorProduto = scanner.nextDouble(); 
+					scanner.nextLine(); 
+					valorValido = true;         
+				} catch (InputMismatchException e) {
+					System.out.println("Valor inválido! Por favor, insira um número válido.");
+					scanner.nextLine(); 
 				}
+			}
+			nomesProdutos.add(nomeProduto);
+			valoresProdutos.add(valorProduto);
 
-				System.out.println("Qual é o valor do produto, caso deseje terminar digite 0");
-				do {
-					valor = ler.nextInt();
+		}
 
-					if(valor != 0) {
-						precos.add(valor);
-					
+		exibirListaCompras(nomesProdutos, valoresProdutos);
+		double valorTotal = calcularValorTotal (valoresProdutos);
+		System.out.println("Valor total a pagar: RS" + String.format("%.2f", valorTotal));
+	}
+	public static void exibirListaCompras (ArrayList<String> nomes, ArrayList<Double> valores) {
+		System.out.println("\nLista de Compras:"); 
+		for (int i=0; i < nomes.size(); i++) {
+			System.out.println(nomes.get(i)+"- R$ " + String.format("%.2f", valores.get(i)));
+		}
+	}
 
-					}
-					
-				
-			
+	public static double calcularValorTotal(ArrayList<Double> valores) {
+		double total = 0;
+		for (double valor : valores) {
+			total += valor;
+		}
+		return total;
+	}
+}
 
-				
-				
 
 
-			
+
+
+
+
+
+
+
+
+
+
+
 
